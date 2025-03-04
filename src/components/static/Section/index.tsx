dfae44dc6225c8d937b51f75d29a5764667c401b
Dreihuhn <flo.luegering@hotmail.de>
2025-03-04 14:04:05 +0100

    fix

--- src/components/static/Section/index.tsx
+++ src/components/static/Section/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 import { Settings } from "@/lib/types";
 import { cva } from "class-variance-authority";
