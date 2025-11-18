bb9c51be2c41169f1618d8244adb336deadbd8fd
Jonas Lörken <jonas.loerken@pechschwarz.com>
2025-11-18 15:25:47 +0100

    fix: Korrekturen

--- src/components/custom/Oxomi/index.tsx
+++ src/components/custom/Oxomi/index.tsx
@@ -18,14 +18,14 @@ export default function Oxomi({ content }: { content: Content }) {
         setTimeout(function () {
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = (window.location.protocol == 'https:' ? 'https:' : 'http:') + '//' + (typeof oxomi_server == 'undefined' ? 'oxomi.com' : oxomi_server) + '/assets/frontend/oxomi.js';
+            script.src = (window.location.protocol == 'https:' ? 'https:' : 'http:') + '//' + (typeof oxomi_server == 'undefined' ? 'oxomi.com' : oxomi_server) + '/assets/frontend/v2/oxomi.js';
             document.getElementsByTagName('head')[0].appendChild(script);
         }, 0);
     }, []);
 
     const params = useSearchParams();
     const query = params.get('query') || '';
-    const supplierNumber = params.get('supplierNumber') || '';
+    const brand = params.get('brand') || '';
 
     return (
         <Section dataComponent="Oxomi" settings={content?.settings}>
@@ -34,7 +34,7 @@ export default function Oxomi({ content }: { content: Content }) {
                     function oxomi_ready() {
                         oxomi.init({ portal: '3000576' });
 
-                        oxomi.portalSearch({
+                        oxomi.universalSearch({
                             target: '#universal-search-output',
                             input: '#universal-search-input',
                             showDetails: true,
@@ -44,18 +44,14 @@ export default function Oxomi({ content }: { content: Content }) {
                             brandsLabel: 'Marken',
                             showDetails: true,
                             query: "${query}",
-                            supplierNumber: "${supplierNumber}"
+                            brand: "${brand}"
                         });
                     }
-
-                    if (typeof oxomi !== 'undefined') {
+                    document.addEventListener('oxomi-loaded', function () {
                         oxomi_ready();
-                    } else {
-                        window.addEventListener('oxomi-ready', oxomi_ready);
-                    }
+                    });
                 `}
             </Script>
-            {supplierNumber}
             <div className="pt-[80px] mb-14">
                 <div className="container">
                     <h1 className="text-h1 leading-tight font-headline text-blue font-light">{content?.headline}</h1>
@@ -64,10 +60,10 @@ export default function Oxomi({ content }: { content: Content }) {
             <div>
                 <div className="container">
                     <div className="grid grid-cols-12 gap-6">
-                        <div className="col-span-4">
+                        <div className="col-span-4 hidden">
                             <div id="universal-search-filter"></div>
                         </div>
-                        <div className="col-span-8">
+                        <div className="col-span-12">
                             <div id="universal-search-input"></div>
                             <div id="universal-search-output">Im Moment gibts noch nichts zu sehen...</div>
                         </div>
