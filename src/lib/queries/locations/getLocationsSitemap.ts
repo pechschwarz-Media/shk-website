b7eda5446fcda3ed81443b495b6fc952eb8837b0
PSMJonas <jonas.loerken@pechschwarz.com>
2026-01-12 10:43:16 +0100

    fix: Sitemap aufbauen

---
+++ src/lib/queries/locations/getLocationsSitemap.ts
@@ -0,0 +1,7 @@
+import { PageMeta } from '@/lib/types';
+
+export default async function getLocationsSitemap() {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}wp/v2/locations?_fields=link,modified,yoast_head_json&per_page=99`);
+    const data: ({ link: string; modified: string } & PageMeta)[] = await response.json();
+    return data;
+}
