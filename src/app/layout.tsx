b92f8ac24a7b0c1cb01ae3e3ab68e45c691b99d0
Jonas Lörken <jonas.loerken@pechschwarz.com>
2026-01-27 13:32:40 +0100

    fix: Tag-Manager einbinden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,25 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     const { isEnabled } = await draftMode();
     return (
         <html lang="de" className="scroll-smooth bg-background-gray">
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('consent', 'default', {
+                                ad_storage: 'denied',
+                                analytics_storage: 'denied',
+                                ad_user_data: 'denied',
+                                ad_personalization: 'denied',
+                                wait_for_update: 500
+                            });
+                            `,
+                    }}
+                />
+            </head>
             <body className={cn(hongkong.variable, panton.variable, 'text-base text-dark font-body pt-20')}>
+                <Script src="https://cloud.ccm19.de/app.js?apiKey=1fa07ed09355d056614058f87d142228edbba2d785876b9f&amp;domain=6978af6f882a491ec4012962" strategy="beforeInteractive" referrerPolicy="origin" />
                 {children}
                 <Marker />
                 <Script id="hubspot" strategy="afterInteractive" src="https://js-eu1.hs-scripts.com/144284637.js" />
