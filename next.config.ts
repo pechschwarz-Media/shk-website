9903147788994db7e8198de7cdf7ceccc1969d75
PSMJonas <jonas.loerken@pechschwarz.com>
2026-02-19 14:02:28 +0100

    fix: Korrekturen

--- next.config.ts
+++ next.config.ts
@@ -26,21 +26,6 @@ const nextConfig: NextConfig = {
         deviceSizes: [576, 768, 992, 1200, 1440, 1920],
     },
     async redirects() {
-        const filePath = path.join(process.cwd(), 'public', 'redirects.json');
-        let jsonRedirects: { source: string; destination: string; permanent: boolean }[] = [];
-
-        try {
-            const fileContent = fs.readFileSync(filePath, 'utf-8');
-            const entries: RedirectEntry[] = JSON.parse(fileContent);
-            jsonRedirects = entries.map((entry) => ({
-                source: entry.old,
-                destination: entry.new,
-                permanent: true,
-            }));
-        } catch (error) {
-            console.error('Fehler beim Laden der redirects.json:', error);
-        }
-
         return [
             {
                 source: '/ausstellungen/',
