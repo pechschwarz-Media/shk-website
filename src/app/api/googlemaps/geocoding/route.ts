dcff447b5edf597d52f1f705f0d2ce93d89bcaf4
Jonas Lörken <jonas.loerken@pechschwarz.com>
2026-03-10 15:17:04 +0100

    fix: Korrekturen

--- src/app/api/googlemaps/geocoding/route.ts
+++ src/app/api/googlemaps/geocoding/route.ts
@@ -2,7 +2,7 @@ export async function POST(request: Request) {
     const { address } = await request.json();
 
     const res = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`,
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&components=country:DE&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`,
         {
             method: 'GET',
         }
