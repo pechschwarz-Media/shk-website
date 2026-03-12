254e0d9065e0cc361e7d31dff58966c1f4d8e241
PSMJonas <jonas.loerken@pechschwarz.com>
2026-03-12 22:48:19 +0100

    Marker.io entfernen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Marker from '@/components/static/Marker/Marker';
 import { cn } from '@/lib/utils';
 import localFont from 'next/font/local';
 import { draftMode } from 'next/headers';
@@ -59,7 +58,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <body className={cn(hongkong.variable, panton.variable, 'text-base text-dark font-body pt-20')}>
                 <Script src="https://cloud.ccm19.de/app.js?apiKey=1fa07ed09355d056614058f87d142228edbba2d785876b9f&amp;domain=6978af6f882a491ec4012962" strategy="beforeInteractive" referrerPolicy="origin" />
                 {children}
-                <Marker />
                 <Script id="hubspot" strategy="afterInteractive" src="https://js-eu1.hs-scripts.com/144284637.js" />
                 {isEnabled && <div className="bg-blue fixed bottom-0 left-0 z-[100] text-small px-8 py-2 rounded-tr-lg text-white">Du befindest dich im Draft-Mode!</div>}
             </body>
