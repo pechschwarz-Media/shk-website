5f63b2e466fb95077b80df29e8415ed1462b27b5
PSMJonas <jonas.loerken@pechschwarz.com>
2026-03-16 13:30:27 +0100

    Korrekturen am API-Key

--- src/app/api/googlemaps/geocoding/route.ts
+++ src/app/api/googlemaps/geocoding/route.ts
@@ -2,12 +2,16 @@ export async function POST(request: Request) {
     const { address } = await request.json();
 
     const res = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&components=country:DE&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`,
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&components=country:DE&key=${
+            process.env.GOOGLE_MAPS_SERVER_API_KEY
+        }`,
         {
             method: 'GET',
         }
     );
     const data = await res.json();
+
+    console.log(data);
 
     return Response.json(data);
 }
