3efcb734b80d856b72bb1308d7fc953755b80493
Jonas Lörken <jonas.loerken@pechschwarz.com>
2026-03-19 14:42:05 +0100

    Korrekturen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,27 +39,40 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     return (
         <html lang="de" className="scroll-smooth bg-background-gray">
             <head>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('consent', 'default', {
-                                ad_storage: 'denied',
-                                analytics_storage: 'denied',
-                                ad_user_data: 'denied',
-                                ad_personalization: 'denied',
-                                wait_for_update: 500
-                            });
-                            `,
-                    }}
-                />
+                <Script data-cookieconsent="ignore" id="cookie-consent-script">{`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag() {
+                            dataLayer.push(arguments);
+                        }
+                        gtag("consent", "default", {
+                            ad_personalization: "denied",
+                            ad_storage: "denied",
+                            ad_user_data: "denied",
+                            analytics_storage: "denied",
+                            functionality_storage: "denied",
+                            personalization_storage: "denied",
+                            security_storage: "granted",
+                            wait_for_update: 500,
+                        });
+                        gtag("set", "ads_data_redaction", true);
+                        gtag("set", "url_passthrough", true);
+                    `}</Script>
                 <Script
                     id="Cookiebot"
                     src="https://consent.cookiebot.com/uc.js"
                     data-cbid="6fa2209f-3255-469c-ac2d-25e094246554"
                     data-blockingmode="auto"
+                    type="text/javascript"
                     strategy="beforeInteractive"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MTCT9SRG');`,
+                    }}
                 />
             </head>
             <body className={cn(hongkong.variable, panton.variable, 'text-base text-dark font-body pt-20')}>
